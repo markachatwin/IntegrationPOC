9af5d445fdbfabed78749e12b6f442a665c43cbf
mark.chatwin@centelon.com <mark.chatwin@centelon.com>
2024-12-20 12:20:12 +1100

    Initial Commit

---
+++ ConvertCSVToJSON.cs
@@ -0,0 +1,42 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Aruma.Integration
+{
+    public class ConvertCSVToJSON
+    {
+        private readonly ILogger<ConvertCSVToJSON> _logger;
+
+        public ConvertCSVToJSON(ILogger<ConvertCSVToJSON> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("ConvertCSVToJSON")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            
+            string data = req.Query["data"];
+            string fileType = req.Query["fileType"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic request = JsonConvert.DeserializeObject(requestBody);
+            data = data ?? request?.data;
+            fileType = fileType ?? request?.fileType;
+            string result = "Testing: " + data.Length + " " + fileType;
+
+            //byte[] byteArray = Encoding.UTF8.GetBytes(data);
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            response.Headers.Add("Content-Type", "text/plain");  
+            response.WriteString(result);
+
+            return response;
+        }        
+    }
+}
